a47c8b99a140f2a5fced025edb3683b7cf210005
Alex-bot-maker-boop <labkovicaleksej62@gmail.com>
2025-12-31 21:43:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiohttp import web
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from aiogram.types import Message, InputFile, FSInputFile  # ← ДОБАВЛЕНО
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
